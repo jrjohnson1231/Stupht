8be2104a7540a15ef819f9224f10123663fa073b
John Johnson <jjohns48@nd.edu>
2016-09-01 02:30:14 -0400

    config cable url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://stupht.herokuapp.com/api/v1/cable'
+  config.action_cable.url = 'wss://stupht.herokuapp.com/api/v1/cable'
   config.action_cable.allowed_request_origins = [ 'https://stupht.herokuapp.com','http://stupht.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
