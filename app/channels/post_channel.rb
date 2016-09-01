10e0ff358a1dacd024ac77e7e58cd868693c2f34
John Johnson <jjohns48@nd.edu>
2016-09-01 02:00:32 -0400

    alert when someone joins channel

--- app/channels/post_channel.rb
+++ app/channels/post_channel.rb
@@ -1,9 +1,10 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class PostChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "'#{params[:email]}_feed'"
+    # stream_from "'#{params[:email]}_feed'"
+    stream_from "'main_feed'"
     ActionCable.server.broadcast(
-      "'#{params[:email]}_feed'",
+      "'main_feed'",
       "#{params[:email]} is listening"
     )
   end
