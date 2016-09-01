90f4fe87ac78429e857db1ede388f384bc1637ea
John Johnson <jjohns48@nd.edu>
2016-09-01 02:32:55 -0400

    alert on opening

--- client/src/app/common/cable.ts
+++ client/src/app/common/cable.ts
@@ -42,7 +42,7 @@ export class Cable {
           observer.next(data);
         } else {
           if (data.type != 'ping') {
-            console.log('received from cable', data);
+            alert('received from cable' + data.message);
           }
         }
       }
